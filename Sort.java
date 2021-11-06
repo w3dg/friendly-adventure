302d3eca0b2d7d6daad9afb30315e9a77b96dbdf
DG <debopamgupta3000@gmail.com>
2021-11-06 11:10:37 +0530

    Add Sort for all 3 algos

--- Sort.java
+++ Sort.java
@@ -1,5 +1,6 @@
 public class Sort {
 
+  // Insertion sort
   static void insertionSort(int arr[]) {
     for (int i = 1; i < arr.length; i++) {
       int key = arr[i];
@@ -13,6 +14,7 @@ public class Sort {
     }
   }
 
+  // Bubble Sort
   static void bubbleSort(int arr[]) {
     for (int i = 0; i < arr.length - 1; i++) {
       for (int j = 0; j < arr.length - i - 1; j++) {
@@ -25,21 +27,39 @@ public class Sort {
     }
   }
 
+  // Selection sort
+  static void selectionSort(int arr[]) {
+    int min;
+    for (int i = 0; i < arr.length - 1; i++) {
+      min = i;
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[min]) {
+          min = j;
+        }
+      }
+      int temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+
   public static void main(String[] args) {
     int n[] = { 55, 235, 373, 23, 3 };
 
     int m[] = new int[5];
+    int p[] = new int[5];
     // Deep copy of array
     for (int i = 0; i < 5; i++) {
       m[i] = n[i];
+      p[i] = n[i];
     }
 
     insertionSort(n);
     bubbleSort(m);
+    selectionSort(p);
 
     for (int i = 0; i < 5; i++) {
-      System.out.println(m[i] + " " + n[i]);
-
+      System.out.println(m[i] + " " + n[i] + " " + p[i]);
     }
   }
 }
