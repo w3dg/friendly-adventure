54bb1dc143b90d940871b60c4672e1e8852cbf17
DG <51162936+debopamgupta@users.noreply.github.com>
2022-03-14 20:01:09 +0530

    Minor Fixes

---
+++ Recursion/FiboRec.java
@@ -0,0 +1,5 @@
+package Recursion;
+
+public class FiboRec {
+
+}
--- java-playground/Fibo.java
+++ java-playground/Fibo.java
@@ -1,3 +1,4 @@
+
 // A class Fibo has been defined to generate the Fibonacci series 0, 1, 1, 2, 3, 5,
 // 8, 13,……. (Fibonacci series are those in which the sum of the previous two
 // terms is equal to the next term).
@@ -34,7 +35,35 @@ fibo(int), and void display( ). Define the main() function to create an object a
 call the functions accordingly to enable the task.
 
 */
+import java.util.*;
 
 public class Fibo {
+  int start, end;
+
+  void read() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter Start and End Limits");
+    start = sc.nextInt();
+    end = sc.nextInt();
+    sc.close();
+  }
+
+  int fibo(int n) {
+    if (n == 0)
+      return 0;
+    if (n == 1)
+      return 1;
+    return fibo(n - 1) + fibo(n - 2);
+  }
+
+  void display() {
+    System.out.println(fibo(5));
+    System.out.println(fibo(3));
+    System.out.println(fibo(10));
+  }
+
+  public static void main(String[] args) {
+    new Fibo().display();
+  }
 
 }
